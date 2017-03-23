05b2a382c6772fd747f767789c95723bae5978a5
Bence Kasza <bk388@cam.ac.uk>
2017-03-23 16:34:37 +0000

    bomb.py created

---
+++ src/bomb.py
@@ -0,0 +1,24 @@
+from ledMatrix import *
+from sense_hat import SenseHat()
+import time
+
+sense = SenseHat()
+sense.set_imu_config(True, True, True)
+
+timer = 0
+
+sense.show_letter(str(timer))
+timer_set = False
+sense.stick.get_events()
+while not timer_set:
+    increase = 0
+    for event in sense.stick.get_events():
+        if event[1] == "right" and event[2] == "pressed":
+            increase ++
+        elif event[1] == "left" and event[2] == "pressed":
+            increase --
+        elif event[1] == "middle" and event[2] == "pressed":
+            timer_set = True
+
+time.sleep(5)
+sense.clear()
