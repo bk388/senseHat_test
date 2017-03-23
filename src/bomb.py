9f344e543f323a7f127ad968e627d65475a1fbbe
Bence Kasza <bk388@cam.ac.uk>
2017-03-23 16:48:49 +0000

    update

--- src/bomb.py
+++ src/bomb.py
@@ -19,10 +19,31 @@ while not timer_set:
             increase -= 1
         elif event[1] == "middle" and event[2] == "pressed":
             timer_set = True
-        print(event)
     timer += increase
     timer = timer%10
     sense.show_letter(str(timer), text_colour=[0, 0, 255])
     
-time.sleep(5)
+time.sleep(0.5)
+
+sense.stick.wait_for_event()
+explode = False
+initTime = time.time()
+deltaTime = 0
+deltaTime_prev = -1
+
+while not explode:
+    deltaTime = time.time() - initTime
+    deltaTime = round(deltaTime, 2)%timer
+    if not deltaTime_prev == deltaTime:
+        deltaTime_prev = deltaTime
+        angle = 2*np.pi*float(deltaTime)/timer + np.pi*int(np.pi)/2 #https://xkcd.com/1275
+        image = getLineImage(4, -angle, 4, [255, 0, 0])
+        try:
+            sense.set_pixels(image)
+        except:
+            print("error")
+            pass
+    if deltaTime == timer:
+        explode = True
+        
 sense.clear()
