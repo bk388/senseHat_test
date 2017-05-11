2d1826035812a1c85b3e361182d236660a8d154e
Bence Kasza <bk388@cam.ac.uk>
2017-05-11 23:05:31 +0100

    ...

--- src/rotateVector.py
+++ src/rotateVector.py
@@ -14,13 +14,13 @@ omega = 1.0
 
 prevTime = time.time()
 deltaTime = 0.0
-"""while len(sense.stick.get_events()) == 0:
+while len(sense.stick.get_events()) == 0:
     deltaTime = time.time() - prevTime
     prevTime = prevTime + deltaTime
     dPhi = omega * deltaTime
     rotMatrix = np.array([[1.0, 0.0, 0.0], [0.0, np.cos(dPhi), -np.sin(dPhi)], [0.0, np.sin(dPhi), np.cos(dPhi)]])
     line = line.dot(rotMatrix)
-    sense.set_pixels(draw3dVector([4, 4], line, [255, 0, 0]))"""
+    sense.set_pixels(draw3dVector([4, 4], line, [255, 0, 0]))
     
 end = False
 while not end:
