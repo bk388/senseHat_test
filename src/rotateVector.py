1e19811fd28a6ade74e7404deb7245bfc021b8b3
Bence Kasza <bk388@cam.ac.uk>
2017-05-11 23:01:09 +0100

    ...

--- src/rotateVector.py
+++ src/rotateVector.py
@@ -7,7 +7,7 @@ sense.set_imu_config(True, True, True)
 
 line = np.array([0.0, 0.0, 5.0])
 
-sense.set_pixels(draw3dVector([0, 0], line, [255, 0, 0]))
+sense.set_pixels(draw3dVector([4, 4], line, [255, 0, 0]))
 sense.stick.get_events()
 
 omega = 1.0
@@ -25,10 +25,17 @@ deltaTime = 0.0
 end = False
 while not end:
     events = sense.stick.get_events()
+    deltaTime = time.time() - prevTime
+    prevTime = prevTime + deltaTime
+    dPhi = omega * deltaTime
     if len(events) != 0:
         for event in events:
             print(event.direction, event.action)
             if event.direction == "middle":
                 end = True
+            elif event.direction == "left" and event.action == "pressed":
+                rotMatrix = np.array([[1.0, 0.0, 0.0], [0.0, np.cos(dPhi), -np.sin(dPhi)], [0.0, np.sin(dPhi), np.cos(dPhi)]])
+                line = line.dot(rotMatrix)
+                sense.set_pixels(draw3dVector([4, 4], line, [255, 0, 0]))
     
 sense.clear()
