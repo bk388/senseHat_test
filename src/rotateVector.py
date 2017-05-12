c5cea814dc87e054e668e33189057065e1166dbb
Bence Kasza <bk388@cam.ac.uk>
2017-05-12 10:26:33 +0100

    ...

--- src/rotateVector.py
+++ src/rotateVector.py
@@ -14,13 +14,13 @@ omega = 1.0
 
 prevTime = time.time()
 deltaTime = 0.0
-while len(sense.stick.get_events()) == 0:
+"""while len(sense.stick.get_events()) == 0:
     deltaTime = time.time() - prevTime
     prevTime = prevTime + deltaTime
     dPhi = omega * deltaTime
     rotMatrix = np.array([[1.0, 0.0, 0.0], [0.0, np.cos(dPhi), -np.sin(dPhi)], [0.0, np.sin(dPhi), np.cos(dPhi)]])
     line = line.dot(rotMatrix)
-    sense.set_pixels(draw3dVector([4, 4], line, [255, 0, 0]))
+    sense.set_pixels(draw3dVector([4, 4], line, [255, 0, 0]))"""
     
 end = False
 while not end:
@@ -31,12 +31,16 @@ while not end:
     dPhi = 0.1
     if len(events) != 0:
         for event in events:
-            print(event.direction, event.action)
+            #print(event.direction, event.action)
             if event.direction == "middle":
                 end = True
             elif event.direction == "left" and event.action == "pressed":
-                print("echo")
-                rotMatrix = np.array([[1.0, 0.0, 0.0], [0.0, np.cos(dPhi), -np.sin(dPhi)], [0.0, np.sin(dPhi), np.cos(dPhi)]])
+                #print("echo")
+                #rotMatrix = np.array([[1.0, 0.0, 0.0], [0.0, np.cos(dPhi), -np.sin(dPhi)], [0.0, np.sin(dPhi), np.cos(dPhi)]])
+                rotMatrix = np.array([[np.cos(dPhi), 0.0, -np.sin(dPhi)], [0.0, 1.0, 0.0], [np.sin(dPhi), 0.0, np.cos(dPhi)]])
+                line = line.dot(rotMatrix)
+            elif event.direction == "right" and event.action == "pressed":
+                rotMatrix = np.array([[np.cos(dPhi), 0.0, np.sin(dPhi)], [0.0, 1.0, 0.0], [-np.sin(dPhi), 0.0, np.cos(dPhi)]])
                 line = line.dot(rotMatrix)
         sense.set_pixels(draw3dVector([4, 4], line, [255, 0, 0]))
     
