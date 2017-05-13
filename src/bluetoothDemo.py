d415b300c4cc81822aa50e3f33b9cc09a4713466
Bence Kasza <bk388@cam.ac.uk>
2017-05-13 19:55:13 +0100

    ...

--- src/bluetoothDemo.py
+++ src/bluetoothDemo.py
@@ -1,6 +1,6 @@
 import bluetooth as bt
        
-devices = bluetooth.discover_devices(duration = 5, lookup_names = True)
+devices = bt.discover_devices(duration = 5, lookup_names = True)
 
 for address, name in devices:
     print(address, name)
