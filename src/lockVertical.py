07487371f96bcdbd83438581e1ca8d50b88677a9
Bence Kasza <bk388@cam.ac.uk>
2017-05-13 12:31:47 +0100

    implemented rotation matrix calculation from yaw, pitch roll

--- src/lockVertical.py
+++ src/lockVertical.py
@@ -1,16 +1,43 @@
 from ledMatrix import *
 from sense_hat import SenseHat
 import time
+from rotateVector import rotMatrix
 
-sense = SenseHat()
-sense.set_imu_config(True, True, True)
-
-vertVect = np.array([0.0, 0.0, 5.0])
-
-sense.set_pixels(draw3dVector([4, 4], vertVect, [255, 0, 0]))
-sense.stick.get_events()
-
-while len(sense.stick.get_events()) == 0:
-    orientation = sense.get_orientation_radians()
-    #rotYaw = np.array([[1.0, 0.0, 0.0], [0.0, np.cos(dPhi), -np.sin(dPhi)], [0.0, np.sin(dPhi), np.cos(dPhi)]])
-    print(orientation["yaw"])
+def main():
+    sense = SenseHat()
+    sense.set_imu_config(True, True, True)
+    
+    vertVect = np.array([0.0, 0.0, 5.0])
+    
+    sense.set_pixels(draw3dVector([4, 4], vertVect, [255, 0, 0]))
+    sense.stick.get_events()
+    
+    while len(sense.stick.get_events()) == 0:
+        orientation = sense.get_orientation_radians()
+        
+        yaw = orientation["yaw"]
+        pitch = orientation["pitch"]
+        roll = orientation["roll"]
+        rotMatrix = getRotMat(yaw, pitch, roll)
+        
+        vec2draw = vertVect.dot(rotMatrix)
+        
+        print(vec2draw)
+        time.sleep(2)
+        
+    sense.clear()
+        
+def getRotMat(yaw, pitch, roll):
+        yawMat = np.array([[np.cos(yaw), -np.sin(yaw), 0.0], 
+                           [np.sin(yaw), np.cos(yaw), 0.0], 
+                           [0.0, 0.0, 1.0]])  
+        pitchMat = np.array([[np.cos(pitch), 0.0, -np.sin(pitch)], 
+                             [0.0, 1.0, 0.0], 
+                             [np.sin(pitch), 0.0, np.cos(pitch)]])
+        rollMat = np.array([[1.0, 0.0, 0.0], 
+                            [0.0, np.cos(roll), -np.sin(roll)], 
+                            [0.0, np.sin(roll), np.cos(roll)]])
+        
+        rotationMatrix = yawMat.dot(pitchMat).dot(rollMat)
+        
+        return rotationMatrix
