72e9b1c26d802a94aa68fe7823895838ab3b04c8
Bence Kasza <bk388@cam.ac.uk>
2017-05-13 12:09:46 +0100

    started using IMU

--- src/lockVertical.py
+++ src/lockVertical.py
@@ -5,4 +5,11 @@ import time
 sense = SenseHat()
 sense.set_imu_config(True, True, True)
 
-vertVect = []
+vertVect = np.array([0.0, 0.0, 5.0])
+
+sense.set_pixels(draw3dVector([4, 4], vertVect, [255, 0, 0]))
+sense.stick.get_events()
+
+while len(sense.stick.get_events()) == 0:
+    orientation = sense.get_orientation_radians()
+    print(orientation)
