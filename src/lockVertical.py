7d2f10f8f127142af1e6c0a61c745eeba547df62
Bence Kasza <bk388@cam.ac.uk>
2017-05-13 12:36:45 +0100

    this should at least run

--- src/lockVertical.py
+++ src/lockVertical.py
@@ -2,32 +2,6 @@ from ledMatrix import *
 from sense_hat import SenseHat
 import time
 
-def main():
-    sense = SenseHat()
-    sense.set_imu_config(True, True, True)
-    
-    vertVect = np.array([0.0, 0.0, 5.0])
-    
-    sense.set_pixels(draw3dVector([4, 4], vertVect, [255, 0, 0]))
-    sense.stick.get_events()
-    
-    while len(sense.stick.get_events()) == 0:
-        orientation = sense.get_orientation_radians()
-        
-        yaw = orientation["yaw"]
-        pitch = orientation["pitch"]
-        roll = orientation["roll"]
-        rotMatrix = getRotMat(yaw, pitch, roll)
-        
-        vec2draw = vertVect.dot(rotMatrix)
-        
-        print(vec2draw)
-        time.sleep(2)
-        
-    sense.clear()
-    
-main()
-        
 def getRotMat(yaw, pitch, roll):
         yawMat = np.array([[np.cos(yaw), -np.sin(yaw), 0.0], 
                            [np.sin(yaw), np.cos(yaw), 0.0], 
@@ -42,3 +16,27 @@ def getRotMat(yaw, pitch, roll):
         rotationMatrix = yawMat.dot(pitchMat).dot(rollMat)
         
         return rotationMatrix
+    
+    
+sense = SenseHat()
+sense.set_imu_config(True, True, True)
+
+vertVect = np.array([0.0, 0.0, 5.0])
+
+sense.set_pixels(draw3dVector([4, 4], vertVect, [255, 0, 0]))
+sense.stick.get_events()
+
+while len(sense.stick.get_events()) == 0:
+    orientation = sense.get_orientation_radians()
+    
+    yaw = orientation["yaw"]
+    pitch = orientation["pitch"]
+    roll = orientation["roll"]
+    rotMatrix = getRotMat(yaw, pitch, roll)
+    
+    vec2draw = vertVect.dot(rotMatrix)
+    
+    print(vec2draw)
+    time.sleep(2)
+    
+sense.clear()
